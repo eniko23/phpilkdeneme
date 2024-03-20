4513980226472d5d827d977be91d52d9a739a139
eniko23 <frtenes@gmail.com>
2024-03-21 01:01:19 +0300

    ilk commit

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<!--
+	ustora by freshdesignweb.com
+	Twitter: https://twitter.com/freshdesignweb
+	URL: https://www.freshdesignweb.com/ustora/
+-->
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ustora Demo</title>
+    
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+   
+
+
+    <div class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="user-menu">
+                        <ul>
+                            
+                        <li><a href="checkout.php"><i class="fa fa-user"></i> Kayıt Ol</a></li>
+                            <li><a href="#"><i class="fa fa-user"></i> Giriş Yap</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="col-md-4">
+                    <div class="header-right">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End header area -->
+    
+    <div class="site-branding-area">
+        <div class="container">
+            <div class="row ">
+                <div class="col-sm-6 icon">
+                    <div class="logo">
+                        <h1><a href="./"><img src="img/logo2.png" width="100" height="100></a></h1>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6">
+                <div class="shopping-item">
+                        <a href="cart.php">SEPET - <span class="cart-amunt"></span> <i class="fa fa-shopping-cart"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End site branding area -->
+    
+    <div class="mainmenu-area">
+        <div class="container">
+            <div class="row">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div> 
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                    <li><a href="index.php">ANASAYFA</a></li>
+                        <li><a href="shop.php">ÜRÜNLER</a></li>
+                        
+                        <li><a href="cart.php">SEPET</a></li>
+                        
+                    </ul>
+                </div>  
+            </div>
+        </div>
+    </div> <!-- End mainmenu area -->
+    
+    <div class="slider-area">
+        	<!-- Slider -->
+			<div class="block-slider block-slider4">
+				<ul class="" id="bxslider-home4">
+					<li>
+						<img src="img/h4-slide1.png" alt="Slide">
+						<div class="caption-group">
+							<h2 class="caption title">
+								TEAM  <span class="primary">FORTRESS <strong>2</strong></span>
+							</h2>
+							<h4 class="caption subtitle">MACERA</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>ŞİMDİ AL</a>
+						</div>
+					</li>
+					<li><img src="img/h4-slide22.png" alt="Slide">
+						<div class="caption-group">
+							<h2 class="caption title">
+								HALF <span class="primary">LIFE<strong>1</strong></span>
+							</h2>
+							<h4 class="caption subtitle">AKSİYON</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>ŞİMDİ AL</a>
+						</div>
+					</li>
+					<li><img src="img/h4-slide33.png" alt="Slide">
+						<div class="caption-group">
+							<h2 class="caption title">
+								COUNTER <span class="primary">STRİKE <strong>1.6</strong></span>
+							</h2>
+							<h4 class="caption subtitle">TAKTİKSEL</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>ŞİMDİ AL</a>
+						</div>
+					</li>
+					<li><img src="img/h4-slide44.png" alt="Slide">
+						<div class="caption-group">
+						  <h2 class="caption title">
+								SONİC<span class="primary">THE <strong>KİRPİ</strong></span>
+							</h2>
+							<h4 class="caption subtitle">EĞLENCE</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>ŞİMDİ AL</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<!-- ./Slider -->
+    </div> <!-- End slider area -->
+    
+    <div class="promo-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo1">
+                        <i class="fa fa-refresh"></i>
+                        <p>30 GÜNDE İADE</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo2">
+                        <i class="fa fa-truck"></i>
+                        <p>ÜCRETSİZ KARGO</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo3">
+                        <i class="fa fa-lock"></i>
+                        <p>GÜVENLİ ÖDEME</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo4">
+                        <i class="fa fa-gift"></i>
+                        <p>YENİ ÜRÜNLER</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End promo area -->
+    
+    
+    
+    <div class="product-widget-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                
+            </div>
+        </div>
+    </div> <!-- End product widget area -->
+    
+    
+    
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="copyright">
+                        <p></p>
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div> <!-- End footer bottom area -->
+   
+    <!-- Latest jQuery form server -->
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+    
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    
+    <!-- jQuery sticky menu -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    
+    <!-- jQuery easing -->
+    <script src="js/jquery.easing.1.3.min.js"></script>
+    
+    <!-- Main Script -->
+    <script src="js/main.js"></script>
+    
+    <!-- Slider -->
+    <script type="text/javascript" src="js/bxslider.min.js"></script>
+	<script type="text/javascript" src="js/script.slider.js"></script>
+  </body>
+</html>
